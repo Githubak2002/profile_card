4f1847c430233239c7d0a1a1b2c52578e92d01c6
Anurag Lohar <109411443+Githubak2002@users.noreply.github.com>
2025-05-15 16:54:58 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ProfileCard from './components/ProfileCard'
-import Info from './components/info'
+import Info from './components/Info'
 
 const App = () => {
   return (
